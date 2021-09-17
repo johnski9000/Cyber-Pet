22d4a84627ba911a0794f84239fca9ec742e9b17
johnski9000 <76655322+johnski9000@users.noreply.github.com>
2021-09-17 09:25:29 +0100

    17/09

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const nameButton = document.getElementById("namebutton");
-const petName = document.getElementById("name");
+const petName = document.getElementById("petname");
 const input = document.getElementById("nameinput");
 const petMood = document.getElementById("mood");
 const petHunger = document.getElementById("hunger");
@@ -12,6 +12,10 @@ const water = document.getElementById("water");
 const petimg1 = document.getElementById("petimg")
 const petimg2 = document.getElementById("petimgg")
 
+// if (petHealth.value == 0) {
+//     alert("GameOver, you let your pet die :(")
+//     window.location.reload();
+// }
 
 // if (petMood.value <= 50){
 //     petimg1.remove();
@@ -27,11 +31,16 @@ play.addEventListener("click", () =>{
     petStats = setInterval(() => {
     petThirst.value -= 2;
     petHunger.value -= 2;
-    petMood.value -= 1;
+    petMood.value -= 2.5;
     if (petThirst.value == 0 && petHunger.value == 0){
         petHealth.value -= 3
     }
-}, 200);
+    if (petHealth.value == 0){
+        clearInterval(petStats);
+        alert("GameOver, you let your pet die :(");
+        window.location.reload();
+    }
+}, 20 );
 })
 
 feed.addEventListener("click", () => {
@@ -43,7 +52,13 @@ water.addEventListener("click", () => {
 pet.addEventListener("click", () => {
     petMood.value += 6;
 })
+// nameButton.addEventListener("click", () => {
+//     petName.textContent = `Name: ${input.value}`;
+//     input.value = "";
+// })
+
 nameButton.addEventListener("click", () => {
-    petName.textContent = `Name: ${input.value}`;
+    petName.textContent = input.value;
     input.value = "";
 })
+
